7d74c4c4b88c57fd9cdab26e213e955d12a2e381
Aleksander Bujnowski <bujnowski.aleksander@gmail.com>
2025-08-30 19:56:54 +0200

    [INIT] env setup

---
+++ src/test/test_packages_import.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision
+import medmnist
+import sklearn
+import matplotlib.pyplot as plt
+from libact.base.dataset import Dataset
+
+print("✅ Wszystkie biblioteki załadowane poprawnie")
